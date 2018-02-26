633d7183c7ae1cf4c540d12e340a5edfa3825101
Axel Bosco <bosa2002@usherbrooke.ca>
2018-02-25 21:45:35 -0500

    Début code app

--- mbed/main.cpp
+++ mbed/main.cpp
@@ -1,13 +1,26 @@
 #include "mbed.h"
+#include <math.h>
 
 DigitalOut led(LED1);
+AnalogIn ADC(p15);
+
+const float COEFFS[6] = {6.7965, -9.5149, 11.0775, -7.0173, 2.1136, -0.2425};
 
 int main() {
-
+	float distance = 0.f;
+	float tension = 0.0f;
+	
 	while(1) {
-		led = 1;
-		wait(1);
-		led = 0;
-		wait(1);
+		tension = (ADC.read_u16() >> 4) * 3.3 / 4096.f;
+		
+		distance = 	(COEFFS[5] * powf(tension, 5.f)) + (COEFFS[4] * powf(tension, 4.f)) +
+								(COEFFS[3] * powf(tension, 3.f)) + (COEFFS[2] * powf(tension, 2.f)) +
+								(COEFFS[1] * tension) + COEFFS[0];
+		
+		distance = 54.f - distance;
+		
+		printf("La position du bras est de : %f cm\n", distance);
+		
+		wait_ms(500);
 	}
 }
