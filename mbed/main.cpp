16e28e1cdeba45cc537457c4cffadaae63294fb5
Axel Bosco <bosa2002@usherbrooke.ca>
2018-02-27 15:18:02 -0500

    Mise à jour main.cpp pour ajouter les commentaires

--- mbed/main.cpp
+++ mbed/main.cpp
@@ -1,3 +1,9 @@
+/******************************
+Auteurs : Axel Bosco et Alexandre Gagnon
+CIPs : bosa2002 et gaga2515
+Date : 28 février 2018
+Description : Calcul de la distance du niveaumètre en fonction d'une tension analogique en entrée.
+******************************/
 #include "mbed.h"
 
 DigitalOut led(LED1);
@@ -7,14 +13,14 @@ const float COEFFS[6] = {6.7965, -9.5149, 11.0775, -7.0173, 2.1136, -0.2425};
 
 int main() {
 	float distance = 0.f;
-	uint16_t tension = 0;
+	float tension = 0.f;
+	uint16_t lecture = 0;
 	
-	int n = powf(2.f,16.f) - 1;
+	int n = powf(2.f,16.f) - 1;	// Valeur maximale de 16 bits.
 	
 	while(1) {
-		
-		tension = ADC.read_u16();
-		tension = (tension / n) * 3.3f;
+		lecture = ADC.read_u16();
+		tension = (lecture * 3.3f ) / n; // Conversion de la lecture 16 bits en tension entre 0 et 3.3 V
 		
 		distance = 	(COEFFS[5] * powf(tension, 5.f)) + (COEFFS[4] * powf(tension, 4.f)) +
 								(COEFFS[3] * powf(tension, 3.f)) + (COEFFS[2] * powf(tension, 2.f)) +
@@ -24,6 +30,6 @@ int main() {
 		
 		printf("La position du bras est de : %f cm\n", distance);
 		
-		wait_ms(500);
+		wait_ms(200);
 	}
 }
