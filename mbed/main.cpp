2d7dc7e0a75d351bf41f890024aaeab547b2ac5a
Axel Bosco <bosa2002@usherbrooke.ca>
2018-02-27 10:59:08 -0500

    Schéma blocs

--- mbed/main.cpp
+++ mbed/main.cpp
@@ -6,13 +6,15 @@ AnalogIn ADC(p16);
 const float COEFFS[6] = {6.7965, -9.5149, 11.0775, -7.0173, 2.1136, -0.2425};
 
 int main() {
-	uint16_t distance = 0;
-	float tension = 0.0f;
+	float distance = 0.f;
+	uint16_t tension = 0;
+	
+	int n = powf(2.f,16.f) - 1;
 	
 	while(1) {
 		
-		tension = ADC.read_u16();		
-		tension = (tension / powf(2.f,16.f)) * 3.3f;
+		tension = ADC.read_u16();
+		tension = (tension / n) * 3.3f;
 		
 		distance = 	(COEFFS[5] * powf(tension, 5.f)) + (COEFFS[4] * powf(tension, 4.f)) +
 								(COEFFS[3] * powf(tension, 3.f)) + (COEFFS[2] * powf(tension, 2.f)) +
